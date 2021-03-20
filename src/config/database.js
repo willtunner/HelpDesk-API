5b3bc5165209fae686447e0ae24c2af187e1f31f
WillTunner <williampereira21@gmail.com>
2021-03-20 15:29:13 -0300

    baby come back

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-dupe-keys */
 module.exports = {
   // ! LOCAL
-  /*
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
@@ -14,17 +13,5 @@ module.exports = {
     underscoredAll: true, // coloca user_id no banco
   },
   // timezone: '-03:00',
-*/
-  // ! HEROKU
-  dialect: 'postgres',
-  host: 'ec2-18-204-101-137.compute-1.amazonaws.com',
-  username: 'lidczdedskhonx',
-  password: 'e13c4019646a8b8ae087513979cdb76d4884c9c6672a3ee9aa39c885103cee1d', // Windows
-  database: 'dfn3bit9i25u9m',
-  define: {
-    timestamps: true, // Quando cria algo coloca o create at e update at
-    underscored: true,
-    underscoredAll: true, // coloca user_id no banco
-  },
-  // timezone: '-03:00',
+
 };
